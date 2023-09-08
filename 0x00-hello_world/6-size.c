25077a11f417d910cf89963c61a4954227dab48e
Sthembile Cebekhulu <cebekhulusthembile@gmail.com>
2023-09-07 19:33:42 -0700

    Prints the size of various types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,6 @@
-#!/bin/bash
 #include <stdio.h>
 /**
- * main - Prints the size of various types on the computer 
+ * main - Prints the size of various types on the computer
  * followed by a new line
  * Return : returns 0 for success
 */
@@ -9,5 +8,12 @@ int main(void)
 {
 	int();
 
-}
+	printf("Size of a char: %c\n", sizeof(char));
+	printf("Size of an int: %d\n", sizeof(int));
+	printf("Size of a long int: %ld\n" sizeof(long));
+	printf("Size of a long long int: %lld\n", sizeof(long long int));
+	printf("Size of a float: %f\n", sizeof(float));
 
+	return (0);
+
+}
