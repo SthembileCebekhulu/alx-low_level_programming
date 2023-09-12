f78e6c13b32286051ed0e881325778ca123a33ac
Sthembile Cebekhulu <cebekhulusthembile@gmail.com>
2023-09-12 10:04:27 -0700

    Variables project

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet in lowercase
+ *
+ * Return: 0 for success
+ *
+*/
+int main()
+{
+	int alpha;
+	
+		for (alpha='a' ;alpha<='z' ;alpha++);
+			putchar(alpha);
+			putchar("\n");
+		return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints all single digits of base 10
+ *
+ * Return: 0 for success
+ *
+*/
+
+int main()
+{
+	int x;
+
+	for(x= '0'; x<= '10'; x++);
+		printf(x);
+
+		return (0);
+}
